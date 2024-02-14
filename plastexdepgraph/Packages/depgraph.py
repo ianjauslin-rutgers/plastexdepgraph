fc7e991b95c669ccbf939f50664dab3171d5477a
Ian Jauslin <ian.jauslin@rutgers.edu>
2024-02-14 17:44:14 -0500

    Specify styles in userdata dir

--- plastexdepgraph/Packages/depgraph.py
+++ plastexdepgraph/Packages/depgraph.py
@@ -29,6 +29,10 @@ want to influence the dependency graph.
   input a plasTeX node and outputting CSS colors for the boundary and interior
   of graph nodes respectively.
 
+* document.userdata['dep_graph']['stylerizer'] can be a function taking as input
+  a plasTeX node and outputting a graphviz style
+  (see https://graphviz.org/docs/attr-types/style/)
+
 * document.userdata['dep_graph']['legend'] can be a list whose entries are pairs
   made of a visual description and an explanation.
   The default value is:
@@ -116,15 +120,7 @@ class DepGraph():
             fillcolor = self.document.userdata['dep_graph'].get('fillcolorizer', lambda x: '')(node)
 
             if fillcolor:
-                # allow for striped fillings when there is more than one color specified
-                if fillcolor.find(':')>=0:
-                    # different for box and ellipse
-                    if item_kind(node)=='box':
-                        style='striped'
-                    else:
-                        style='wedged'
-                else:
-                    style='filled'
+                style = self.document.userdata['dep_graph'].get('stylerizer', lambda x: 'filled')(node)
 
                 graph.add_node(node.id,
                                label=node.id.split(':')[-1],
@@ -133,10 +129,11 @@ class DepGraph():
                                color=color,
                                fillcolor=fillcolor)
             else:
+                style = self.document.userdata['dep_graph'].get('stylerizer', lambda x: '')(node)
                 graph.add_node(node.id,
                                label=node.id.split(':')[-1],
                                shape=shapes.get(item_kind(node), 'ellipse'),
-                               style='',
+                               style=style,
                                color=color)
         for s, t in self.edges:
             if s in self.nodes and t in self.nodes:
